6a73e2bee9458d1549d2202eb78a3696efe8f088
ameen <ameen.izhac@gmail.com>
2022-10-14 11:25:31 +0100

    [ai20] [dev] created some basic structure in the simple user page

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,6 @@ export default function Navbar({isSuper, link, href}) {
         <div className="Navbar">
             <nav className='navbar'>
                 <label className='logo'>AI VidGen {isSuper ? <p className="superTag">super</p> : null} </label>
-
                 <li className="pageLink"><a className="link" href={href}>{link}</a></li>
             </nav>
         </div>
