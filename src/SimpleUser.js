c37d184b276f859447d23f0b90b2aaa50bbf780a
ameen <ameen.izhac@gmail.com>
2022-10-14 08:59:37 +0100

    [ai20] [dev] navbar is mostly complete, focus will now be on creating the main two pages simple user and super user

--- src/SimpleUser.js
+++ src/SimpleUser.js
@@ -3,8 +3,8 @@ import Navbar from './Navbar'
 export default function SimpleUser() {
     return (
         <>
-            <Navbar link="Super User" href="superUser"/>
-            <p>dw this is the simple user page</p>
+            <Navbar isSuper={false} link="Super User" href="superUser"/>
+            <input></input>
         </>
     );
   }
--- src/SuperUser.js
+++ src/SuperUser.js
@@ -3,8 +3,8 @@ import Navbar from './Navbar'
 export default function SuperUser() {
     return (
         <>
-            <Navbar link="Simple User" href="/" />
-            <p>dw this is the simple user page</p>
+            <Navbar isSuper={true} link="Simple User" href="/" />
+            <input></input>
         </>
     );
   }
