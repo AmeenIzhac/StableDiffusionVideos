b58de92d094a54f147d76f148279ec492829f84f
Raaif <hraaif@yahoo.com>
2022-10-26 22:16:30 +0100

    [rh1320, ii120, ai20] pass prompt to server

--- client/src/SimpleUser.js
+++ client/src/SimpleUser.js
@@ -4,12 +4,17 @@ import axios from 'axios';
 
 export default function SimpleUser() {
     const [src, setSrc] = useState("");
+    const [prompt, setPrompt] = useState("");
 
-    useEffect(() => {
+    const handleChange = (e) => {
+        setPrompt(e.target.value);
+    }
+
+    function getVideo() {
         axios({
             method: 'get',
-            url: 'http://109.158.65.154:8080/api',
-            responseType: 'blob'
+            url: `http://109.158.65.154:8080/api?prompt=${prompt}`,
+            responseType: 'blob',
         })
         .then((response) => {
             setSrc(URL.createObjectURL(response.data));
@@ -17,6 +22,11 @@ export default function SimpleUser() {
         .catch((error) => {
             console.log(error);
         });
+        setPrompt("");
+    };
+
+    useEffect(() => {
+        getVideo();
     }, []);
     
     if (src !== "")  {
@@ -25,7 +35,8 @@ export default function SimpleUser() {
                 <Navbar isSuper={false} link="Super User" href="superUser"/>
                 <div className="mainDiv">
                     <div  className='promptDiv'>
-                        <input className='prompt' placeholder='Enter Text Prompt...'></input>
+                        <input className='prompt' value={prompt} placeholder='Enter Text Prompt...' onChange={handleChange}></input>
+                        <button className='promptButton' onClick={getVideo}>Generate Video</button>
                     </div>
                     <div className='videoDiv'>
                         <video id="vidObj" width="500" height="360" controls loop muted autoPlay>
--- server/server.py
+++ server/server.py
@@ -11,11 +11,11 @@ def index():
 
 @app.route('/api')
 def api():
-    # get video from filesystem
-    video = open('test.mp4', 'rb').read()
+    args = request.args
+    prompt = args.get('prompt')
+    print(prompt)
     return send_file('./test.mp4', mimetype='video/mp4')
-    # data = {'prompt': 'my prompt', 'video': video}
-    # return jsonify(data, 200)
+
 
 
 if __name__ == '__main__':
