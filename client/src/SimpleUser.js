dedef8d7d5f87f767ce363e18cea43dfd7e02308
Raaif <hraaif@yahoo.com>
2022-11-04 00:53:36 +0000

    [rh1320,ii120,ai20,jh20,is922] connect pipeline

--- client/src/SimpleUser.js
+++ client/src/SimpleUser.js
@@ -1,61 +1,68 @@
 import Navbar from './Navbar'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
+import loadingAnimation from './assets/painting.gif';
 
 export default function SimpleUser() {
     const [src, setSrc] = useState("");
     const [prompt, setPrompt] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const handleChange = (e) => {
         setPrompt(e.target.value);
     }
-
-    var extension  =  (prompt==="") ? '' : `?prompt=${prompt}`;
-    var url = 'http://109.158.65.154:8080/api' + extension;
-
     function getVideo() {
+
+        if (prompt === "") {
+            alert("Please enter a prompt");
+            return;
+        }
+
+        setLoading(true);
         axios({
             method: 'get',
-            url: url,
+            url: `http://109.158.65.154:8080/api?prompt=` + prompt,
             responseType: 'blob',
-	    accessControlAllowOrigin: '*',
-            timeout: 300000
+            timeout: 10000000,
         })
         .then((response) => {
+            console.log(response.data)
             setSrc(URL.createObjectURL(response.data));
             console.log(src);
         })
         .catch((error) => {
             console.log(error);
+        })
+        .finally(() => {
+            setLoading(false);
         });
+
         setPrompt("");
     };
-
-    useEffect(() => {
-        getVideo();
-    }, []);
     
-    if (src !== "")  {
-        return (
-            <div className='SimpleUser'>
-                <Navbar isSuper={false} link="Super User" href="superUser"/>
-                <div className="mainDiv">
-                    <div  className='promptDiv'>
-                        <input className='prompt' value={prompt} placeholder='Enter Text Prompt...' onChange={handleChange}></input>
-                        <button className='promptButton' onClick={getVideo}>Generate Video</button>
-                    </div>
-                    <div className='videoDiv'>
+    return (
+        <div className='SimpleUser'>
+            <Navbar isSuper={false} link="Super User" href="superUser"/>
+            <div className="mainDiv">
+                <div  className='promptDiv'>
+                    <input className='prompt' value={prompt} placeholder='Enter Text Prompt...' onChange={handleChange}></input>
+                    <button className='promptButton' onClick={getVideo}>Generate Video</button>
+                </div>
+                <div className='videoDiv'>
+                    {src ? 
                         <video id="vidObj" width="500" height="360" controls loop muted autoPlay>
                             <source src={src} type="video/mp4"/>
-                        </video>
-                    </div>
-                    <div>
-                        <p> ${src} </p>
-                    </div>
+                        </video> 
+                        : 
+                        (loading ? 
+                            <img src={loadingAnimation} alt='loading thingy'/> : null)
+                    }
+                    
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+
   }
 
   
--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
-from os import sendfile
+import os
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
+import subprocess
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -13,10 +14,17 @@ def index():
 def api():
     args = request.args
     prompt = args.get('prompt')
-    print(prompt)
-    return send_file('./test.mp4', mimetype='video/mp4')
+    video_name = str(prompt).replace(" ", "_")
+    # os.system("conda activate ldm")
+    os.system(f'python ../stable-diffusion-2/optimizedSD/optimized_txt2video.py --prompt "{prompt}" --video_name "{video_name}" --H 512 --W 512 --scale 10 --ddim_steps 40 --sampler ddim --strength 0.38 --frames 30 --fps 15 --zoom 1.02 --turbo --ckpt \'../stable-diffusion-2/models/ldm/stable-diffusion-v1/model.ckpt\' --config \'../stable-diffusion-2/optimizedSD/v1-inference.yaml\' ')
+    # os.system("conda deactivate")
+    return send_file(
+         f'./outputs/txt2vid-samples/videos/{video_name}.mp4',
+        #'./test.mp4',
+        mimetype='video/mp4')
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.1.125', port=8080)
+    # ssl_context = ('./certicates/server.crt', './certicates/server.key')
+    app.run(debug=True, host='192.168.1.126', port=8080)
