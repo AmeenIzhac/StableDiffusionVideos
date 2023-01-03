207231748845a122c2f789163f912ba1c8480c85
AmeenIzhac <ameen.izhac@gmail.com>
2023-01-03 15:47:12 +0000

    [ai20] video now goes to google cloud under a folder unique to the currently logged in user

--- client/src/SimpleUser.js
+++ client/src/SimpleUser.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import { useRef, useState } from "react";
 import axios from "axios";
 import loadingAnimation from "./assets/loading.gif";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 import Dropdown from "./Dropdown";
 import FrameSelect from "./components/FrameSelect";
 import a from "./assets/a.jpg";
@@ -12,39 +12,42 @@ import d from "./assets/d.jpg";
 
 export default function SimpleUser() {
   const [src, setSrc] = useState("");
-  const promptRef = useRef()
+  const promptRef = useRef();
   const [isImg2Img, setisImg2Img] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [frames, setFrames] = useState("60")
-  const [width, setWidth] = useState("512")
-  const [height, setHeight] = useState("512")
-  const [angle, setAngle] = useState("0")
-  const [zoom, setZoom] = useState("1.1")
-  const [fps, setFps] = useState("20")
-  const [xShift, setxShift] = useState("0")
-  const [yShift, setyShift] = useState("0")
-  const [noNoises, setNoNoises] = useState("1")
-  const [loggedIn, setLoggedIn] = useState(Cookies.get("loggedInUser") != null)
+  const [frames, setFrames] = useState("60");
+  const [width, setWidth] = useState("512");
+  const [height, setHeight] = useState("512");
+  const [angle, setAngle] = useState("0");
+  const [zoom, setZoom] = useState("1.1");
+  const [fps, setFps] = useState("20");
+  const [xShift, setxShift] = useState("0");
+  const [yShift, setyShift] = useState("0");
+  const [noNoises, setNoNoises] = useState("1");
+  const [loggedIn, setLoggedIn] = useState(Cookies.get("loggedInUser") != null);
 
   //we dont need a state to track the upscaling
   //just use document.getElementById('upscale').checked which will return a boolean
 
-  const [prompts, setPrompts] = useState([])
-  const [progress, setProgress] = useState(0)
+  const [prompts, setPrompts] = useState([]);
+  const [progress, setProgress] = useState(0);
   var jobID;
-  var fileName = ""
+  var fileName = "";
 
   // Create a new job on server and set the current jobID
   function createJob() {
-    const prompt = promptRef.current.value
-    fileName = [...prompts, prompt][0].replace(" ", "_")
+    const prompt = promptRef.current.value;
+    fileName = [...prompts, prompt][0].replace(" ", "_");
     setLoading(true);
     axios({
       method: "get",
-      url: `https://stablediffusionvideoswebserver-production.up.railway.app/request`,
-      // url: `http://localhost:3001/request`,
+      // url: `https://stablediffusionvideoswebserver-production.up.railway.app/request`,
+      url: `http://localhost:3001/request`,
       params: {
-        prompts: (prompt.length == 0 ? [...prompts] : [...prompts, prompt]).join(";"),
+        prompts: (prompt.length == 0
+          ? [...prompts]
+          : [...prompts, prompt]
+        ).join(";"),
         frames: frames,
         width: width,
         height: height,
@@ -55,13 +58,13 @@ export default function SimpleUser() {
         yShift: yShift,
         noNoises: noNoises,
         isImg2Img: isImg2Img,
-        upscale: document.getElementById('upscale').checked
+        upscale: document.getElementById("upscale").checked,
       },
       responseType: "application/json",
-      timeout: 10000
+      timeout: 10000,
     })
       .then((res) => {
-        jobID = JSON.parse(res.data).id
+        jobID = JSON.parse(res.data).id;
         console.log(jobID);
         poll();
       })
@@ -98,16 +101,16 @@ export default function SimpleUser() {
 
   // get status of job
   async function getJobStatus() {
-    console.log(jobID)
+    console.log(jobID);
     return axios({
       method: "get",
-      url: `https://stablediffusionvideoswebserver-production.up.railway.app/status`,
-      // url: `http://localhost:3001/status`,
+      // url: `https://stablediffusionvideoswebserver-production.up.railway.app/status`,
+      url: `http://localhost:3001/status`,
       params: {
-        jobID: jobID
+        jobID: jobID,
       },
       responseType: "text",
-      timeout: 10000
+      timeout: 10000,
     })
       .then((res) => {
         console.log(JSON.parse(res.data));
@@ -117,20 +120,24 @@ export default function SimpleUser() {
         console.log(err);
         return "error";
       });
-
   }
 
   function getCreatedVideo() {
+    var user = "undefined";
+    if (typeof Cookies.get("loggedInUser") != "undefined") {
+      user = Cookies.get("loggedInUser");
+    }
     axios({
       method: "get",
-      url: `https://stablediffusionvideoswebserver-production.up.railway.app/getCreatedVideo`,
-      // url: `http://localhost:3001/getCreatedVideo`,
+      // url: `https://stablediffusionvideoswebserver-production.up.railway.app/getCreatedVideo`,
+      url: `http://localhost:3001/getCreatedVideo`,
       params: {
         jobID: jobID,
-        fileName: fileName
+        fileName: fileName,
+        user: user,
       },
       responseType: "blob",
-      timeout: 10000
+      timeout: 10000,
     })
       .then((res) => {
         console.log(res);
@@ -143,20 +150,19 @@ export default function SimpleUser() {
       });
   }
 
-
-
   // temp for testing
   const framesT = [a, b, c, d];
   const selectFunction = (index) => {
     console.log(`Frame ${index} Selected`);
-  }
+  };
   const getNewFrame = () => {
     console.log("Get new frame");
-  }
+  };
   // end temp testing
 
   function getVideo() {
-    const prompt = promptRef.current.value
+    console.log("get viddddddeo");
+    const prompt = promptRef.current.value;
 
     if (prompt === "") {
       alert("Please enter a prompt");
@@ -166,8 +172,8 @@ export default function SimpleUser() {
     setLoading(true);
     axios({
       method: "get",
-      url: `https://stablediffusionvideoswebserver-production.up.railway.app/generate`,
-      // url: `http://localhost:3001/generate`,
+      // url: `https://stablediffusionvideoswebserver-production.up.railway.app/generate`,
+      url: `http://localhost:3001/generate`,
       params: {
         prompts: [...prompts, prompt].join(";"),
         frames: frames,
@@ -180,10 +186,10 @@ export default function SimpleUser() {
         yShift: yShift,
         noNoises: noNoises,
         isImg2Img: isImg2Img,
-        upscale: document.getElementById('upscale').checked
+        upscale: document.getElementById("upscale").checked,
       },
       responseType: "blob",
-      timeout: 10000000
+      timeout: 10000000,
     })
       .then((response) => {
         console.log(response.data);
@@ -197,17 +203,17 @@ export default function SimpleUser() {
         setLoading(false);
       });
 
-    setAngle("0")
-    setZoom("1")
-    setWidth("704")
-    setHeight("704")
+    setAngle("0");
+    setZoom("1");
+    setWidth("704");
+    setHeight("704");
 
     promptRef.current.value = "";
-    setPrompts([])
-    const dropdown = document.getElementById("dropdown")
-    const button = document.getElementById("button")
-    dropdown.classList.remove("open")
-    button.classList.remove("rotate")
+    setPrompts([]);
+    const dropdown = document.getElementById("dropdown");
+    const button = document.getElementById("button");
+    dropdown.classList.remove("open");
+    button.classList.remove("rotate");
   }
 
   function addPrompt() {
@@ -215,31 +221,30 @@ export default function SimpleUser() {
       alert("Please enter a prompt");
       return;
     }
-    setPrompts([...prompts, promptRef.current.value])
+    setPrompts([...prompts, promptRef.current.value]);
     promptRef.current.value = "";
   }
 
   //dropdown code
   const dropOptions = () => {
-    const dropdown = document.getElementById("dropdown")
-    const button = document.getElementById("button")
-    dropdown.classList.toggle("open")
-    button.classList.toggle("rotate")
-  }
+    const dropdown = document.getElementById("dropdown");
+    const button = document.getElementById("button");
+    dropdown.classList.toggle("open");
+    button.classList.toggle("rotate");
+  };
 
   const slideStateChange = (e, set) => {
     set(e.target.value);
-  }
+  };
 
   function handleKeyDown(event) {
-    if (event.key === 'Enter') {
-      getVideo()
+    if (event.key === "Enter") {
+      getVideo();
     }
   }
 
-
   return (
-    <div className='SimpleUser'>
+    <div className="SimpleUser">
       <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Dropdown
         frames={frames}
@@ -266,41 +271,71 @@ export default function SimpleUser() {
         dropOptions={dropOptions}
       />
       <div className="mainDiv">
-        <div className='videoDiv'>
-          {src ?
-            <video id="vidObj" width="500" height="360" controls loop muted autoPlay>
+        <div className="videoDiv">
+          {src ? (
+            <video
+              id="vidObj"
+              width="500"
+              height="360"
+              controls
+              loop
+              muted
+              autoPlay
+            >
               <source src={src} type="video/mp4" />
             </video>
-            :
-            (loading ?
-              <div className="loading">
-                <img className="loading" src={loadingAnimation} alt='loading thingy' />
-                <div className="progress-bar">
-                  <div className="progress" style={{ width: `${progress * 100}%` }}></div>
-                </div>
-                <p style={{ color: `var(--main-bg-light)` }}> Progress: {Math.round(progress * 10000) / 100}%</p>
+          ) : loading ? (
+            <div className="loading">
+              <img
+                className="loading"
+                src={loadingAnimation}
+                alt="loading thingy"
+              />
+              <div className="progress-bar">
+                <div
+                  className="progress"
+                  style={{ width: `${progress * 100}%` }}
+                ></div>
               </div>
-
-              : null)
-          }
+              <p style={{ color: `var(--main-bg-light)` }}>
+                {" "}
+                Progress: {Math.round(progress * 10000) / 100}%
+              </p>
+            </div>
+          ) : null}
         </div>
-        <div className='promptContainerDiv'>
-          <div className='promptDiv'>
-            <input className='prompt' ref={promptRef} placeholder='Enter Text Prompt...' onSubmit={createJob} onKeyDown={handleKeyDown}></input>
-            <button className='promptButton' onClick={addPrompt}>+</button>
-            <button className='promptButton' onClick={createJob}>Generate Video</button>
+        <div className="promptContainerDiv">
+          <div className="promptDiv">
+            <input
+              className="prompt"
+              ref={promptRef}
+              placeholder="Enter Text Prompt..."
+              onSubmit={createJob}
+              onKeyDown={handleKeyDown}
+            ></input>
+            <button className="promptButton" onClick={addPrompt}>
+              +
+            </button>
+            <button className="promptButton" onClick={createJob}>
+              Generate Video
+            </button>
             {/* <button className='promptButton' onClick={getVideo} onSubmit={logger}>Generate Video</button> */}
           </div>
           <div className="promptsContainer">
             {prompts.map((prompt, index) => {
-              return <div key={index} className="promptsList">
-                <span> {prompt} </span>
-                <button
-                  onClick={() => { setPrompts(prompts.filter((_, i) => i !== index)) }}
-                  className="removePrompt">
-                  <div className="horizontal"></div>
-                </button>
-              </div>
+              return (
+                <div key={index} className="promptsList">
+                  <span> {prompt} </span>
+                  <button
+                    onClick={() => {
+                      setPrompts(prompts.filter((_, i) => i !== index));
+                    }}
+                    className="removePrompt"
+                  >
+                    <div className="horizontal"></div>
+                  </button>
+                </div>
+              );
             })}
           </div>
         </div>
@@ -310,5 +345,4 @@ export default function SimpleUser() {
       </div>
     </div>
   );
-
 }
