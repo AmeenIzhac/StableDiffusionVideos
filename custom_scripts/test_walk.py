f031c1150147446d91ee9e8f8552f35b9e00c6b9
Raaif <hraaif@yahoo.com>
2023-01-05 15:33:08 +0000

    [is922] small fixes and adjustments

--- custom_scripts/test_walk.py
+++ custom_scripts/test_walk.py
@@ -11,19 +11,18 @@ video_path = 'outputs/videos'
 model_state = load_model(path_args, optimized=True)
 
 #video_args.prompts = ["a very old 19th century phone", "an old 1930s phone", "a fix 1980s phone", "a 1990s portable phone", "a 2000s nokia cellphone", "a modern iphone"]
-video_args.prompts = ["a savana landscape, in africa, with wild animals, beautiful photography, artstation 4k", 
-                        "a jungle landscape, in africa, rainforest with wild animals, beautiful photography, artstation 4k"]
+video_args.prompts = ["the observable universe"]
 
 
-image_args.steps = 40
-image_args.W = 768
+image_args.steps = 15
+image_args.W = 512
 image_args.H = 512
 image_args.scale = 10
 video_args.upscale = True
 
-video_args.frames = 30
+video_args.frames = 10
 video_args.fps = 24
-video_args.exp = 2
-video_args.video_name = "africa_many_interp.mp4"
+video_args.interp_exp = 3
+video_args.video_name = "space.mp4"
 path_args.video_path = os.path.join(video_path, video_args.video_name)
-generate_walk_video(image_args, video_args, path_args, model_state)
+generate_walk_video(image_args, video_args, path_args, model_state, n_noises=2)
