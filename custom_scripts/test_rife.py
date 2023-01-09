472352be6832f0c03e1d8adc02859bb1dfc0ee44
TalosSources <ismail.sahbane@epfl.ch>
2023-01-09 19:35:48 +0100

    [is922] fix conflicts

--- custom_scripts/test_rife.py
+++ custom_scripts/test_rife.py
@@ -3,5 +3,5 @@ sys.path.append('ECCV2022-RIFE')
 
 from inference_rife import motion_interpolation
 
-motion_interpolation('outputs/images', 'outputs/videos/africa_smooth.mp4', fps=30, frames_count=30, starting_frame=63, exp=2, scale=1.0, codec='avc1')
+motion_interpolation('outputs/images', 'outputs/videos/quoient.mp4', fps=30, frames_count=24, starting_frame=0, exp=2, scale=1.0, codec='avc1')
 
