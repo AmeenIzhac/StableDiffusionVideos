146bbfd76923a5381ba7a2a10e38587352139f5e
Ibrahim Ibrahim <ii83326@gmail.com>
2023-01-09 22:26:00 +0000

    [ii120] cleanup some small bugs

--- client/src/Img2ImgOptions.js
+++ client/src/Img2ImgOptions.js
@@ -20,25 +20,25 @@ export default function Img2ImgOptions({
           <p className="tooltip">Angle : <span id="demo">{angle}</span>
             <span class="tooltiptext">The angle of rotation between 2 frames in degrees</span>
             </p>
-          <input type="range" min="-10" step="0.1" max="10" value={angle} className='slider' id="myRange" onChange={e => slideStateChange(e, setAngle)} />
+          <input type="range" min="-20" step="0.1" max="20" value={angle} className='slider' id="myRange" onChange={e => slideStateChange(e, setAngle)} />
       </div>
       <div className='alignCenter'>
           <p className="tooltip">Zoom : <span id="demo">{zoom}</span>
             <span class="tooltiptext">The zoom factor between 2 frames. Smaller than 1 is zoom out, larger is zoom in, 1 is no zoom</span>
             </p>
-          <input type="range" min="0.7" step="0.001" max="1.3" value={zoom} className='slider' id="myRange" onChange={e => slideStateChange(e, setZoom)} />
+          <input type="range" min="0.5" step="0.001" max="1.5" value={zoom} className='slider' id="myRange" onChange={e => slideStateChange(e, setZoom)} />
       </div>
       <div className='alignCenter'>
-          <p className="tooltip">x-shift : <span id="demo">{xShift}</span>
+          <p className="tooltip">Horizontal shift : <span id="demo">{xShift}</span>
               <span class="tooltiptext">The horizontal translation (in pixels) between 2 frames</span>
               </p>
-          <input type="range" min="-10" step="0.1" max="10" value={xShift} className='slider' id="myRange" onChange={e => slideStateChange(e, setxShift)} />
+          <input type="range" min="-50" step="0.1" max="50" value={xShift} className='slider' id="myRange" onChange={e => slideStateChange(e, setxShift)} />
       </div>
       <div className='alignCenter'>
-          <p className="tooltip">y-shift : <span id="demo">{yShift}</span>
+          <p className="tooltip">Vertical shift : <span id="demo">{yShift}</span>
             <span class="tooltiptext">The vertical translation (in pixels) between 2 frames</span>
             </p>
-          <input type="range" min="-10" step="0.1" max="10" value={yShift} className='slider' id="myRange" onChange={e => slideStateChange(e, setyShift)} />
+          <input type="range" min="-50" step="0.1" max="50" value={yShift} className='slider' id="myRange" onChange={e => slideStateChange(e, setyShift)} />
       </div>
       <div className='alignCenter'>
           <p className="tooltip">Strength (advanced) : <span id="demo">{strength}</span>
