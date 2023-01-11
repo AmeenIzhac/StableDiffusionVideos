53fc87e2f098adb35cad206523076d6fc0523854
TalosSources <ismail.sahbane@epfl.ch>
2023-01-11 18:31:39 +0100

    [is922] make python server file management more robust

--- server/server.py
+++ server/server.py
@@ -9,6 +9,8 @@ from txt2video import load_model, generate_video, generate_walk_video, ImageArgs
 
 # Load model
 path_args = PathArgs()
+path_args.image_path = './outputs/frames'
+path_args.video_path = './outputs/videos'
 model = load_model(path_args, optimized=True)
 
 app = Flask(__name__)
@@ -34,7 +36,7 @@ def getVideo():
     for arg in args:
         print(arg, args[arg])
     file_name = args.get('fileName').replace(' ', '_')
-    file_path = os.path.abspath(f'./outputs/videos/{file_name}.mp4')
+    file_path = os.path.abspath(f'{os.path.join(path_args.video_path, file_name)}.mp4')
     return send_file(file_path, mimetype='video/mp4')
 
 
@@ -56,6 +58,7 @@ def api():
     image_args.H = int(args.get('height')) 
 
     video_args = VideoArgs()
+    video_args.video_name = video_name
 
     video_args.prompts = prompts
 
@@ -74,10 +77,14 @@ def api():
     video_args.strength = float(args.get('strength'))
 
     video_args.upscale = bool(args.get('upscale'))
-    video_args.video_name = video_name
 
-    path_args.image_path = os.path.abspath('./outputs/frames/')
-    path_args.video_path = os.path.abspath(f'./outputs/videos/{video_name}.mp4')
+    def empty_dir(dir):
+        for file in os.listdir(dir):
+            os.remove(os.path.join(dir, file))
+
+    #delete the previous one
+    empty_dir(path_args.image_path)
+    empty_dir(path_args.video_path)
 
     # Generate video
     if args.get('isImg2Img') == 'true':
@@ -87,7 +94,6 @@ def api():
 
     # Respond with the video contents
     return send_file(path_args.video_path, mimetype='video/mp4')
-
 
 if __name__ == '__main__':
     # ssl_context = ('./certicates/server.crt', './certicates/server.key')
